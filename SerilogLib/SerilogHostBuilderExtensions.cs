430a8de8255f3d8ba8eec736d0d93e11cde14e47
Neph <philippe.balleydier@gmail.com>
2026-02-22 00:26:10 +0100

    add serilog lib

---
+++ SerilogLib/SerilogHostBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace SerilogLib
+{
+	public static class SerilogHostBuilderExtensions
+	{
+		public static IHostBuilder UseSerilog(this IHostBuilder hostBuilder)
+		{
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Information()
+				.Enrich.FromLogContext()
+				.WriteTo.Console()
+				.WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+				.CreateLogger();
+
+			return hostBuilder.UseSerilog();
+		}
+	}
+}
