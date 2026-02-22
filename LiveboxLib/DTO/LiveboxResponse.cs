a727093efb6ca3bea9b8f7882f248fa2dc2f9f1d
Neph <philippe.balleydier@gmail.com>
2026-02-22 12:20:53 +0100

    fix not required fields

--- LiveboxLib/DTO/LiveboxResponse.cs
+++ LiveboxLib/DTO/LiveboxResponse.cs
@@ -3,7 +3,7 @@ namespace LiveboxLib.DTO;
 public class LiveboxResponse
 {
 	public int Error { get; set; }
-	public required string Description { get; set; }
-	public required string Info { get; set; }
+	public string? Description { get; set; }
+	public string? Info { get; set; }
 	public required Dictionary<string, object> Data { get; set; }
 }
